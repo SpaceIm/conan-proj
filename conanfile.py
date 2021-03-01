0520cbbec1e4894b57c8a430455712d934ca5bdf
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-03-01 14:26:29 +0100

    synchronize with CCI

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ import os
 
 from conans import ConanFile, CMake, tools
 
-required_conan_version = ">=1.28.2"
+required_conan_version = ">=1.28.0"
 
 class ProjConan(ConanFile):
     name = "proj"
@@ -40,20 +40,20 @@ class ProjConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
+        if tools.Version(self.version) < "7.0.0":
+            del self.options.with_tiff
+            del self.options.with_curl
 
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
 
     def requirements(self):
-        self.requires("sqlite3/3.34.0")
-        if self.options.with_tiff:
-            self.requires("libtiff/4.1.0")
-        if self.options.with_curl:
+        self.requires("sqlite3/3.34.1")
+        if self.options.get_safe("with_tiff"):
+            self.requires("libtiff/4.2.0")
+        if self.options.get_safe("with_curl"):
             self.requires("libcurl/7.74.0")
-
-    def build_requirements(self):
-        self.build_requires("sqlite3/3.34.0")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -74,19 +74,25 @@ class ProjConan(ConanFile):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
-        self._cmake.definitions["ENABLE_TIFF"] = self.options.with_tiff
-        self._cmake.definitions["ENABLE_CURL"] = self.options.with_curl
-        self._cmake.definitions["BUILD_TESTING"] = False
         self._cmake.definitions["USE_THREAD"] = self.options.threadsafe
-        self._cmake.definitions["ENABLE_IPO"] = False
         self._cmake.definitions["BUILD_CCT"] = self.options.build_executables
         self._cmake.definitions["BUILD_CS2CS"] = self.options.build_executables
         self._cmake.definitions["BUILD_GEOD"] = self.options.build_executables
         self._cmake.definitions["BUILD_GIE"] = self.options.build_executables
         self._cmake.definitions["BUILD_PROJ"] = self.options.build_executables
         self._cmake.definitions["BUILD_PROJINFO"] = self.options.build_executables
-        self._cmake.definitions["BUILD_PROJSYNC"] = self.options.build_executables and self.options.with_curl
         self._cmake.definitions["PROJ_DATA_SUBDIR"] = "res"
+        if tools.Version(self.version) < "7.0.0":
+            self._cmake.definitions["PROJ_TESTS"] = False
+            self._cmake.definitions["BUILD_LIBPROJ_SHARED"] = self.options.shared
+            self._cmake.definitions["ENABLE_LTO"] = False
+            self._cmake.definitions["JNI_SUPPORT"] = False
+        else:
+            self._cmake.definitions["ENABLE_TIFF"] = self.options.with_tiff
+            self._cmake.definitions["ENABLE_CURL"] = self.options.with_curl
+            self._cmake.definitions["BUILD_TESTING"] = False
+            self._cmake.definitions["ENABLE_IPO"] = False
+            self._cmake.definitions["BUILD_PROJSYNC"] = self.options.build_executables and self.options.with_curl
         self._cmake.configure()
         return self._cmake
 
@@ -98,9 +104,13 @@ class ProjConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        # TODO: also define deprecated PROJ4::proj alias?
-        self.cpp_info.names["cmake_find_package"] = "PROJ"
-        self.cpp_info.names["cmake_find_package_multi"] = "PROJ"
+        proj_version = tools.Version(self.version)
+        cmake_config_filename = "proj" if proj_version >= "7.0.0" else "proj4"
+        cmake_namespace = "PROJ" if proj_version >= "7.0.0" else "PROJ4"
+        self.cpp_info.filenames["cmake_find_package"] = cmake_config_filename
+        self.cpp_info.filenames["cmake_find_package_multi"] = cmake_config_filename
+        self.cpp_info.names["cmake_find_package"] = cmake_namespace
+        self.cpp_info.names["cmake_find_package_multi"] = cmake_namespace
         self.cpp_info.components["projlib"].names["cmake_find_package"] = "proj"
         self.cpp_info.components["projlib"].names["cmake_find_package_multi"] = "proj"
         self.cpp_info.components["projlib"].libs = tools.collect_libs(self)
@@ -108,16 +118,17 @@ class ProjConan(ConanFile):
             self.cpp_info.components["projlib"].system_libs.append("m")
             if self.options.threadsafe:
                 self.cpp_info.components["projlib"].system_libs.append("pthread")
-        if self.settings.os == "Windows":
-            self.cpp_info.components["projlib"].system_libs.append("shell32")
-            if tools.Version(self.version) >= "7.1.0":
+        elif self.settings.os == "Windows":
+            if proj_version >= "7.0.0":
+                self.cpp_info.components["projlib"].system_libs.append("shell32")
+            if proj_version >= "7.1.0":
                 self.cpp_info.components["projlib"].system_libs.append("Ole32")
         if not self.options.shared and tools.stdcpp_library(self):
             self.cpp_info.components["projlib"].system_libs.append(tools.stdcpp_library(self))
         self.cpp_info.components["projlib"].requires.append("sqlite3::sqlite3")
-        if self.options.with_tiff:
+        if self.options.get_safe("with_tiff"):
             self.cpp_info.components["projlib"].requires.append("libtiff::libtiff")
-        if self.options.with_curl:
+        if self.options.get_safe("with_curl"):
             self.cpp_info.components["projlib"].requires.append("libcurl::libcurl")
         if self.options.shared and self.settings.compiler == "Visual Studio":
             self.cpp_info.components["projlib"].defines.append("PROJ_MSVC_DLL_IMPORT")
